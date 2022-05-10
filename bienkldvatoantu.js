547846fdc5c88862151b7603ba8a8189ebc95b4a
6Thienthien6 <thien502760@gmail.com>
2022-05-10 16:07:08 +0700

    d2

--- bienkldvatoantu.js
+++ bienkldvatoantu.js
@@ -1,3 +1,4 @@
+//baitap 1
 //Inputhoahoc = prompt('nhập điểm hóa học');
 //Inputvatly = prompt('nhập điểm vật vật lý');
 //Inputsinhhoc = prompt('nhập điểm sinh học');
@@ -5,8 +6,24 @@
 //let h = parseFloat(Inputhoahoc);
 //let s = parseFloat(Inputsinhhoc);
 //dtb = (v + h + s) / 3;
-dt = v + h + s
-document.write("Điểm tổng =" + dt)
-    //document.write("Điểm trung bình =" + dtb); //baitap 1
+//dt = v + h + s
+//document.write("Điểm tổng =" + dt)
+//document.write("Điểm trung bình =" + dtb); 
 
 //*baitap 2
+//InputCelius = prompt('nhập độ C')
+//let Celius = parseInt(InputCelius)
+//let Fahrenheit = (Celius / 5) * 9 + 32;
+//alert('Fahrenheit :' + Fahrenheit);
+
+//Baitap 3
+//Inputbankinh = prompt('nhập bán kính')
+//let bankinh = parseInt(Inputbankinh)
+//let dientich = bankinh * bankinh * 3.14;
+//alert('dientich :' + dientich); 
+
+//Baitap 4
+inputbankinh = prompt('nhập bán kính')
+let bankinh = parseInt(inputbankinh)
+let chuvi = bankinh * 3.14;
+alert('chuvi:' + chuvi);
