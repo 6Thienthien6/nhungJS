f0cd579b2be965c10258df1309b07c4beb382b5a
6Thienthien6 <thien502760@gmail.com>
2022-05-10 14:01:24 +0700

    day2

---
+++ jstoantu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the height");
+let witdth = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = witdth * height;
+document.write("the area is:" + area);
