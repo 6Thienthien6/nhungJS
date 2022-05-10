bb8f02b3e569564c62ac446ca27e52047b5ee8e0
6Thienthien6 <thien502760@gmail.com>
2022-05-10 15:34:57 +0700

    d2

---
+++ bienkldvatoantu.js
@@ -0,0 +1,12 @@
+//Inputhoahoc = prompt('nhập điểm hóa học');
+//Inputvatly = prompt('nhập điểm vật vật lý');
+//Inputsinhhoc = prompt('nhập điểm sinh học');
+//let v = parseFloat(Inputvatly);
+//let h = parseFloat(Inputhoahoc);
+//let s = parseFloat(Inputsinhhoc);
+//dtb = (v + h + s) / 3;
+dt = v + h + s
+document.write("Điểm tổng =" + dt)
+    //document.write("Điểm trung bình =" + dtb); //baitap 1
+
+//*baitap 2
