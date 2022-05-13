8d263127356562de1ceb9fb50504e29c997af629
6Thienthien6 <thien502760@gmail.com>
2022-05-13 08:35:51 +0700

    130522

---
+++ nanam.js
@@ -0,0 +1,5 @@
+let a = 5
+let b = 8
+let c = 20
+let ptb2 = `${a}x2 + ${b}x + ${c} = 0`;
+console.log(ptb2)
---
+++ tinhtoan.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 8;
+let c = 20;
+let d = 7
+    //let bt = `${a}sin2 + ${b}cos2 + ${c}Sincos = ${d}`
+    //console.log(bt)
+let all = `${b}^2 - ${a}^2 = (${b} - ${a}) * (${b} + ${a})`
+console.log(all)
